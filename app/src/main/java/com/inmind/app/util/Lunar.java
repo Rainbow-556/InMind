0c03387dee3f35ab5c5a4c079c3cf2517e65f2fc
lixiang <903263494@qq.com>
2017-01-11 11:18:13 +0800

    1.提交工具类

---
+++ app/src/main/java/com/inmind/app/util/Lunar.java
@@ -0,0 +1,13 @@
+package com.inmind.app.util;
+
+/**
+ * Created by lixiang on 2017/1/10.
+ * 农历bean
+ */
+public class Lunar{
+    /**是否为闰月*/
+    public boolean isLeap;
+    public int lunarDay;
+    public int lunarMonth;
+    public int lunarYear;
+}
---
+++ app/src/main/java/com/inmind/app/util/Solar.java
@@ -0,0 +1,11 @@
+package com.inmind.app.util;
+
+/**
+ * Created by lixiang on 2017/1/10.
+ * 公历bean
+ */
+public class Solar{
+    public int solarDay;
+    public int solarMonth;
+    public int solarYear;
+}
