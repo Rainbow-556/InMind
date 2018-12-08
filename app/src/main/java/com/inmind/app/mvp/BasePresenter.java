286936076b0ede9507eeb3b27a7e207067962273
Rainbow-556 <903263494@qq.com>
2018-12-08 15:39:27 +0800

    1.使用动态代理实现在调用IView的方法时省略判断view是否为空的逻辑

--- app/src/main/java/com/inmind/app/mvp/BasePresenter.java
+++ app/src/main/java/com/inmind/app/mvp/BasePresenter.java
@@ -1,15 +1,19 @@
 package com.inmind.app.mvp;
 
 import java.lang.ref.WeakReference;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * Created by lixiang on 2018/11/27.<br/>
  */
 public class BasePresenter<V extends IView> implements IPresenter {
     private WeakReference<IView> mViewRef;
+    private V mProxyView;
 
     protected V getView() {
-        return mViewRef != null ? (V) mViewRef.get() : null;
+        return mProxyView;
     }
 
     @Override
@@ -18,6 +22,16 @@ public class BasePresenter<V extends IView> implements IPresenter {
             mViewRef.clear();
         }
         mViewRef = new WeakReference<>(view);
+        // 使用动态代理实现在调用IView的方法时省略判断view是否为空的逻辑
+        mProxyView = (V) Proxy.newProxyInstance(view.getClass().getClassLoader(), view.getClass().getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (mViewRef != null && mViewRef.get() != null) {
+                    return method.invoke(mViewRef.get(), args);
+                }
+                return null;
+            }
+        });
     }
 
     @Override
